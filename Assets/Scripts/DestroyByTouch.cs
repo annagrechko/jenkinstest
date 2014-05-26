26f011aec934612994cfc0a1bb88a3e8fd1d2d56
Anna Grechko <anna.grechko@tatemgames.com>
2014-05-26 13:18:13 +0300

    repo update

---
+++ Assets/Scripts/DestroyByTouch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTouch : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	private GAmeController gameController;
+	public int scoreValue;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GAmeController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GAmeController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+			return; 
+
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
+		gameController.AddScore (scoreValue);
+
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/GAmeController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GAmeController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 spawnValue;
+	public int hazartCount;
+
+	public float spawnStart;
+	public float spawnWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText gameOverText;
+	public GUIText restartText;
+
+	private bool gameOver;
+	private bool restartGame;
+
+	private int score;
+
+	void Start()
+	{
+		gameOver = false;
+		restartGame = false; 
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update()
+	{
+		if ((restartGame) &&  (Input.GetKeyDown (KeyCode.R)))
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (spawnStart);
+		while (true) 
+		{
+			for (int i = 0; i< hazartCount; i++) {
+					Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+					Quaternion spawnRotation = Quaternion.identity;
+					Instantiate (hazard, spawnPosition, spawnRotation);
+					yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restartGame = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int scoreValue)
+	{
+		score += scoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score; 
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game over!";
+		gameOver = true;
+	}
+}
